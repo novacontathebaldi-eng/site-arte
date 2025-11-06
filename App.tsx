0b6e7894ad03ea845096afb63f8dd0b7e465528c
othebaldi <compras.thebaldi@gmail.com>
2025-11-06 20:31:23 -0300

    ok

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import { CartProvider } from './context/CartContext';
 import { ToastProvider } from './context/ToastContext';
@@ -36,7 +36,7 @@ const App: React.FC = () => {
       <AuthProvider>
         <ToastProvider>
           <CartProvider>
-            <BrowserRouter>
+            <HashRouter>
               <div className="flex flex-col min-h-screen font-body bg-background text-text-primary">
                 <Toaster />
                 <Header />
@@ -97,7 +97,7 @@ const App: React.FC = () => {
                 </main>
                 <Footer />
               </div>
-            </BrowserRouter>
+            </HashRouter>
           </CartProvider>
         </ToastProvider>
       </AuthProvider>
